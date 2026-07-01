ee07c1a1c6b206198f8a33489c0ee73f9f86355f
Alok Kumar Gupta <trooooperr@gmail.com>
2026-07-01 20:29:38 +0530

    fix: allow private network preflight requests in print-agent CORS headers by setting Access-Control-Allow-Private-Network to true

--- print-agent/print-agent.js
+++ print-agent/print-agent.js
@@ -125,6 +125,7 @@ app.use((req, res, next) => {
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Private-Network', 'true');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
